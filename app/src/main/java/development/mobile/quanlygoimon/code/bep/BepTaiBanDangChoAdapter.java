225c2f0d7bdf4f5077b0d1536113023070e233bd
PhucTho <phanphuctho7760@gmail.com>
2018-04-25 08:32:10 +0700

    Add adapter tai ban dang cho

---
+++ app/src/main/java/development/mobile/quanlygoimon/code/bep/BepTaiBanDangChoAdapter.java
@@ -0,0 +1,11 @@
+package development.mobile.quanlygoimon.code.bep;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+public class BepTaiBanDangChoAdapter extends ArrayAdapter {
+    public BepTaiBanDangChoAdapter(@NonNull Context context, int resource, int textViewResourceId) {
+        super(context, resource, textViewResourceId);
+    }
+}
