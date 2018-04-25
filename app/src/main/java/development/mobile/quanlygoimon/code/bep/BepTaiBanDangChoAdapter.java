4dfeee6e14f3dded0e110d3cb992f20d3ccdd358
PhucTho <phanphuctho7760@gmail.com>
2018-04-25 11:48:25 +0700

    them code BepTaiBanDangChoAdapter

--- app/src/main/java/development/mobile/quanlygoimon/code/bep/BepTaiBanDangChoAdapter.java
+++ app/src/main/java/development/mobile/quanlygoimon/code/bep/BepTaiBanDangChoAdapter.java
@@ -1,11 +1,48 @@
 package development.mobile.quanlygoimon.code.bep;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class BepTaiBanDangChoAdapter extends ArrayAdapter {
-    public BepTaiBanDangChoAdapter(@NonNull Context context, int resource, int textViewResourceId) {
-        super(context, resource, textViewResourceId);
+import java.util.ArrayList;
+
+import development.mobile.quanlygoimon.code.R;
+import development.mobile.quanlygoimon.code.entity.ChiTietHoaDon;
+
+public class BepTaiBanDangChoAdapter extends ArrayAdapter<ChiTietHoaDon> {
+
+    Activity context = null;
+    ArrayList<ChiTietHoaDon> myArray = null;
+    int layoutId;
+
+    public BepTaiBanDangChoAdapter(@NonNull Activity context, int resource, @NonNull ArrayList<ChiTietHoaDon> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.myArray = objects;
+        this.layoutId = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        convertView = inflater.inflate(layoutId, null);
+
+        if (myArray.size() > 0 && position >= 0) {
+            ChiTietHoaDon cthd = myArray.get(position);
+            final TextView tvIdHD = (TextView)convertView.findViewById(R.id.tv_idhd_item_taiban_dangcho);
+            tvIdHD.setText(cthd.getMaHoaDon().toString());
+            final TextView tvTenMonAn = (TextView)convertView.findViewById(R.id.tv_tenmonan_item_taiban_dangcho);
+            tvTenMonAn.setText(cthd.getTenMonAn().toString());
+            final TextView tvSoLuong = (TextView)convertView.findViewById(R.id.tv_soluong_item_taiban_dangcho);
+            tvTenMonAn.setText(cthd.getSoLuong());
+        }
+        return convertView;
     }
 }
