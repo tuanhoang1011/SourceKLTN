737a9d9bc995e7ae3bc933e77201ab611afc48c7
PhucTho <phanphuctho7760@gmail.com>
2018-05-07 12:21:48 +0700

    unit testing BepActivity (1)

--- app/src/test/java/development/mobile/quanlygoimon/code/bep/taiban/TaiBanDangChoFragmentTest.java
+++ app/src/test/java/development/mobile/quanlygoimon/code/bep/taiban/TaiBanDangChoFragmentTest.java
@@ -1,10 +1,37 @@
 package development.mobile.quanlygoimon.code.bep.taiban;
 
+import android.test.AndroidTestCase;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.junit.Test;
 
-public class TaiBanDangChoFragmentTest {
+import java.lang.reflect.Method;
+
+import development.mobile.quanlygoimon.code.bep.dattruoc.DatTruocDanhSachFragment;
+
+public class TaiBanDangChoFragmentTest extends AndroidTestCase{
+
+
 
     @Test
-    public void onCreateView() {
+    public void testConnectFirebase() throws Throwable{
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        Method method = DatTruocDanhSachFragment.class.getDeclaredMethod("getAllPhieuDatTruoc");
+        method.setAccessible(true);
+//        return method.invoke(targetObject, argObjects);
+
+//        Method method = targetClass.getDeclaredMethod(methodName, argClasses);
+//        method.setAccessible(true);
+//        return method.invoke(targetObject, argObjects);
+
+//        Field field = targetClass.getDeclaredField(fieldName);
+//        field.setAccessible(true);
+//        field.set(object, value);
+
+//        assertNotNull();
     }
 }
